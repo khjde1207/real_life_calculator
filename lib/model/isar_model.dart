4a5a4644151a7d7875f058c97188427577fcc94b
자유해결사 <khjde1207@gmail.com>
2022-12-09 16:26:40 +0900

    데이터 구조 테스트

---
+++ lib/model/isar_model.dart
@@ -0,0 +1,16 @@
+
+
+
+// import 'package:isar/isar.dart';
+
+// @Collection()
+// class SettingIsar {
+
+//   @enumerated
+//   List<PageData> tabpages = [];
+// }
+
+// enum PageData {
+//   PAGE1,
+//   PAGE2,
+// }
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -5,6 +5,19 @@ import 'package:get/get.dart';
 class MainPage extends GetView {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Text("sasafdsafdfdsaf"),
+      bottomNavigationBar: NavigationBar(destinations: [
+        NavigationDestination(icon: Icon(Icons.calculate), label: "계산기"),
+        NavigationDestination(icon: Icon(Icons.calculate), label: "퍼센트"),
+        NavigationDestination(icon: Icon(Icons.calculate), label: "환율"),
+        NavigationDestination(icon: Icon(Icons.calculate), label: "주식"),
+        NavigationDestination(icon: Icon(Icons.calculate), label: "임금"),
+        NavigationDestination(icon: Icon(Icons.calculate), label: "이자"),
+        NavigationDestination(icon: Icon(Icons.calculate), label: "날자/나이"),
+        NavigationDestination(icon: Icon(Icons.calculate), label: "세계시간"),
+        NavigationDestination(icon: Icon(Icons.calculate), label: "설정"),
+      ]),
+    );
   }
 }
